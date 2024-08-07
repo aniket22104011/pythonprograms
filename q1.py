7b866553f1bbb7ee90465a09081d62125db22ef4
aniket22104011 <22104011@mail.jiit.ac.in>
2024-08-07 09:58:02 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,5 @@
+fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
+print("The first fruit is:", fruits[0])
+print("The last fruit is:", fruits[-1])
+middle_fruits = fruits[1:4]
+print("The middle three fruits are:", middle_fruits)
